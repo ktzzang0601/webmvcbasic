b361d81543029780b19c33a111b447b3e0626e68
Park Ki Tae <kitae.park@balky.kr>
2019-07-24 17:07:32 +0900

    Initial commit

---
+++ src/main/java/me/kt/webmvcbasic/WebmvcbasicApplication.java
@@ -0,0 +1,13 @@
+package me.kt.webmvcbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmvcbasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebmvcbasicApplication.class, args);
+    }
+
+}
